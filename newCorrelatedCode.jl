e6419137ad9d82a886c63b85eaa86397f1312a7b
Vitor Farinha Luz <63761242+vfarinhaluz@users.noreply.github.com>
2022-03-01 18:03:25 -0800

    add correlated code

--- newCorrelatedCode.jl
+++ newCorrelatedCode.jl
@@ -7,13 +7,14 @@
 - linear projection
 - plot
  =#
+ using LazySets, Plots, LinearAlgebra
 
 struct SetofActionDistributions
     S::LazySet{N} where N
     normalform::NormalForm
 
-    function SetofActionDistributions(S, normalform)
-        dim(S)==prod(normalform.nMoves) ? new(S, normalform) : error("Incompatible inputs")
+    function SetofActionDistributions(S::LazySet{N} where N, normalform::NormalForm)
+        dim(S)==prod(normalform.nMoves) ? new(S, normalform) : error("Inputs with incompatible dimensions.")
     end
 
 end
@@ -35,6 +36,54 @@ function matrixCalcPayoff1(recommended::Int64, deviation::Int64, normalform::Nor
     return swapedMat[:]
 end
 
+function polyhedronIC_1(normalform::NormalForm)
+    arrayOfHalfspacesDeviation = [
+        matrixCalcPayoff1(i, j, normalform) - matrixCalcPayoff1(i, i, normalform)
+        for i in 1:normalform.nMoves[1], j in 1:normalform.nMoves[1]
+    ]
+
+    halfSpaceVector=HalfSpace.(
+        arrayOfHalfspacesDeviation[ findall(!iszero, arrayOfHalfspacesDeviation) ],
+        0.
+    )
+    return SetofActionDistributions(HPolytope(halfSpaceVector), normalform) 
+end
+
+### IC Player 2
+
+function matrixCalcPayoff2(recommended::Int64, deviation::Int64, normalform::NormalForm)
+    payoff2=normalform.payoffMatList[2]
+    swapedMat = [
+                            (i[2]==recommended).*payoff2[deviation,i[1]] for i in CartesianIndices(payoff2)
+    ]
+    return swapedMat[:]
+end
+
+function polyhedronIC_2(normalform::NormalForm)
+    arrayOfHalfspacesDeviation = [
+        matrixCalcPayoff2(i, j, normalform) - matrixCalcPayoff2(i, i, normalform)
+        for i in 1:normalform.nMoves[2], j in 1:normalform.nMoves[2]
+    ]
+
+    halfSpaceVector=HalfSpace.(
+        arrayOfHalfspacesDeviation[ findall(!iszero, arrayOfHalfspacesDeviation) ],
+        0.
+    )
+    return SetofActionDistributions(HPolytope(halfSpaceVector), normalform)  
+end
+
+
+### Correlated Equilibrium polytope
+
+function CorrEqPolytope(normalform::NormalForm)
+    probSet=PolyhedronProbConstraints(normalform).S
+    IC1_Set=polyhedronIC_1(normalform).S
+    IC2_Set=polyhedronIC_2(normalform).S
+
+    return SetofActionDistributions(probSet ∩ IC1_Set ∩ IC2_Set, normalform ) 
+end
+
+
 
 #= TO DO:
 - Complete matrix to check deviations of player 1
@@ -43,9 +92,3 @@ end
 - Build intersection
 - projection in 2 dim
 - Plot =#
-
-
-function Polyhedron_IC_1(normalform::NormalForm)
-    payoff1=normalform.payoffMatList[1]
-    return nothing
-end
